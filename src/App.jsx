37c5c9138d10ff3fcdf9f60a1d276397bd9faa76
vijayadharshini <vijayadharshinikrishnasamy19@gmail.com>
2026-01-11 15:25:14 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+
+
+function App() {
+  return (  <>
+  
+  <button class="btn btn-success">login</button>
+  </>
+);
+}
+
+export default App
---
+++ src/header.jsx
@@ -0,0 +1,4 @@
+function Header(name){
+    return <h2>Hello, {name.name}</h2>
+}
+export default Header;
